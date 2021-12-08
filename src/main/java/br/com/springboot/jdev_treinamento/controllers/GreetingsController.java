9d3b96dc96a0c6f7a9431bb88231731204b9173f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2021-12-08 15:57:41 -0300

    Criando o END-POINT para salvar no banco

--- src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,36 +24,47 @@ import br.com.springboot.jdev_treinamento.repository.UsuarioRepository;
  */
 @RestController
 public class GreetingsController {
-
+	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-
+	
 	@RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String greetingText(@PathVariable String name) {
 		return "Curso Spring Boot API: " + name + "!";
 
 	}
-
+	
 	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String retornaOlaMundo(@PathVariable String nome) {
-
+		
 		Usuario usuario = new Usuario();
 		usuario.setNome(nome);
-
+		
 		usuarioRepository.save(usuario);
-
+		
 		return "Ola Mundo! " + nome;
 	}
-
+	
+	//-------------------------------------------------------
+	
 	@GetMapping(value = "listatodos")
 	@ResponseBody
-	public ResponseEntity<List<Usuario>> listaUsuario() {
-
+	public ResponseEntity<List<Usuario>> listaUsuario(){
+		
 		List<Usuario> usuarios = usuarioRepository.findAll();
-
+		
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
-
+	
+	@PostMapping(value = "salvar") /* Mapeia a URL */
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ /* Recebe os dados para salvar*/
+		
+		Usuario user = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+	
 }
