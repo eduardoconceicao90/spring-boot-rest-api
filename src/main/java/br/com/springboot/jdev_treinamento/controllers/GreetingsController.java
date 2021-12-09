004a7701def0c0181e4046ccd47e4a7b69503822
Eduardo Conceição <eduardosaconceicao@gmail.com>
2021-12-09 14:19:05 -0300

    Criando END-POINT para DELETE do banco

--- src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,47 +26,59 @@ import br.com.springboot.jdev_treinamento.repository.UsuarioRepository;
  */
 @RestController
 public class GreetingsController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String greetingText(@PathVariable String name) {
 		return "Curso Spring Boot API: " + name + "!";
 
 	}
-	
+
 	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String retornaOlaMundo(@PathVariable String nome) {
-		
+
 		Usuario usuario = new Usuario();
 		usuario.setNome(nome);
-		
+
 		usuarioRepository.save(usuario);
-		
+
 		return "Ola Mundo! " + nome;
 	}
-	
-	//-------------------------------------------------------
-	
+
+	// -------------------------------------------------------
+
+	/* LISTAR */
 	@GetMapping(value = "listatodos")
 	@ResponseBody
-	public ResponseEntity<List<Usuario>> listaUsuario(){
-		
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+
 		List<Usuario> usuarios = usuarioRepository.findAll();
-		
+
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
-	
+
+	/* SALVAR */
 	@PostMapping(value = "salvar") /* Mapeia a URL */
 	@ResponseBody /* Descrição da resposta */
-	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ /* Recebe os dados para salvar*/
-		
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) { /* Recebe os dados para salvar */
+
 		Usuario user = usuarioRepository.save(usuario);
-		
+
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 	}
-	
+
+	/* DELETAR */
+	@DeleteMapping(value = "delete") /* Mapeia a URL */
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<String> delete(@RequestParam Long iduser) { /* Recebe os dados para salvar */
+
+		usuarioRepository.deleteById(iduser);
+
+		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+	}
+
 }
