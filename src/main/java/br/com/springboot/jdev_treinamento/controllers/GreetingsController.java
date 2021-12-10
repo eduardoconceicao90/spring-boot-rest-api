e29901ea11a433878693282cdfb0b61f3bbb2bb2
Eduardo Conceição <eduardosaconceicao@gmail.com>
2021-12-10 09:23:00 -0300

    Criando END-POINT para buscar e atualizar no banco

--- src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -70,15 +71,38 @@ public class GreetingsController {
 
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 	}
+	
+	/* ATUALIZAR */
+	@PutMapping(value = "atualizar") /* Mapeia a URL */
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) { 
+		
+		if (usuario.getId() == null) {
+			return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);
+		}
+		
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+	}
 
 	/* DELETAR */
-	@DeleteMapping(value = "delete") /* Mapeia a URL */
+	@DeleteMapping(value = "deletar") /* Mapeia a URL */
 	@ResponseBody /* Descrição da resposta */
-	public ResponseEntity<String> delete(@RequestParam Long iduser) { /* Recebe os dados para salvar */
+	public ResponseEntity<String> deletar(@RequestParam Long iduser) {
 
 		usuarioRepository.deleteById(iduser);
 
 		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+	}
+	
+	/* BUSCAR */
+	@GetMapping(value = "buscar") /* Mapeia a URL */
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<Usuario> buscar(@RequestParam(name = "iduser") Long iduser) { /* Recebe os dados para consultar */
+
+		Usuario usuario = usuarioRepository.findById(iduser).get();
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
 
 }
