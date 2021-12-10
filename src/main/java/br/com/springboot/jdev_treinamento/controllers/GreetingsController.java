e6a687a77f8499d3bc8f6c10ed07c1e2975baaf2
Eduardo Conceição <eduardosaconceicao@gmail.com>
2021-12-10 16:34:29 -0300

    Definindo o contexto da aplicação

--- src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
@@ -7,50 +7,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.jdev_treinamento.model.Usuario;
 import br.com.springboot.jdev_treinamento.repository.UsuarioRepository;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
 @RestController
 public class GreetingsController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	@RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String greetingText(@PathVariable String name) {
-		return "Curso Spring Boot API: " + name + "!";
-
-	}
-
-	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String retornaOlaMundo(@PathVariable String nome) {
-
-		Usuario usuario = new Usuario();
-		usuario.setNome(nome);
-
-		usuarioRepository.save(usuario);
-
-		return "Ola Mundo! " + nome;
-	}
-
-	// -------------------------------------------------------
+	/* ----------- C R U D ----------- */
 
 	/* LISTAR */
 	@GetMapping(value = "listatodos")
@@ -96,11 +69,12 @@ public class GreetingsController {
 		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
 	}
 
+	// -----------------------------------------------
+
 	/* BUSCAR POR ID */
-	@GetMapping(value = "buscar") /* Mapeia a URL */
+	@GetMapping(value = "buscarpoid") /* Mapeia a URL */
 	@ResponseBody /* Descrição da resposta */
-	public ResponseEntity<Usuario> buscar(
-			@RequestParam(name = "iduser") Long iduser) { /* Recebe os dados para consultar */
+	public ResponseEntity<Usuario> buscarpoid(@RequestParam(name = "iduser") Long iduser) { /* Recebe os dados para consultar */
 
 		Usuario usuario = usuarioRepository.findById(iduser).get();
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
